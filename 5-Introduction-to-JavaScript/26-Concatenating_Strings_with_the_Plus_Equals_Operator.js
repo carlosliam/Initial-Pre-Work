b1bf717511e3e6de588d9364d4ab4283d7d4d8b7
Carlos Liam <carlos@aarzee.me>
2019-04-04 12:59:46 -0700

    Add lesson 5-26 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// I was previously familiar with using the += operator to concatenate strings.
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
