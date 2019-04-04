8806f95a7d168eb10bdc2b5f9b565896e1363e9e
Carlos Liam <carlos@aarzee.me>
2019-04-04 13:07:55 -0700

    Add lesson 5-33 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// I was previously familiar with subtracting from string.length in brackets.
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
