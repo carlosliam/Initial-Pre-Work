14f75a992519044d0ecbc2bb50c65c5da9657a32
Carlos Liam <carlos@aarzee.me>
2019-04-04 13:11:02 -0700

    Add lesson 5-35 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+// I was previously familiar with functions and arguments.
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb ;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
