9327f079a298de4959bb9174a61ecd349468f60e
Carlos Liam <carlos@aarzee.me>
2019-04-04 13:16:08 -0700

    Add lesson 5-39 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,11 @@
+// I was previously familiar with using bracket notation to modify array items.
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
