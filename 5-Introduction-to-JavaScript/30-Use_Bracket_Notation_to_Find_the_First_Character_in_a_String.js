ce0585fd3c90dcc17110d9932969836f48a0fa3c
Carlos Liam <carlos@aarzee.me>
2019-04-04 13:03:15 -0700

    Add lesson 5-30 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+// I was previously familiar with the bracket notation to select characters from a string.
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
