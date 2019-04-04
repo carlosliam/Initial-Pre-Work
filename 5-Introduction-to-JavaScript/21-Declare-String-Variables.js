485d7b47150957c0734b937de0c567d4d98b9f4b
Carlos Liam <carlos@aarzee.me>
2019-04-04 12:54:26 -0700

    Add lesson 5-21 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// I was previously familiar with string literals in JS.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Carlos";
+var myLastName = "Bello";
