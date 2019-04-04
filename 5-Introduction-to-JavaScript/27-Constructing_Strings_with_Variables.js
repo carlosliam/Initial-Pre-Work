26c073dcb460416e6b44824adcd761c09928dbed
Carlos Liam <carlos@aarzee.me>
2019-04-04 13:00:43 -0700

    Add lesson 5-27 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// I was previously familiar with using variables to concatenate strings.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Carlos Bello";
+var myStr = "My name is " + myName + " and I am well!";
