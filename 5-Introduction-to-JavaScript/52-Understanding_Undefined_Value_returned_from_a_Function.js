ddb07463e845bcf30025997240177e952bbe8294
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:02:51 -0700

    Add lesson 5-52 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+// I was previously familiar with undefined as an implicit return value.
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+var returnedValue = addFive();
