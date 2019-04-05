1d4d9abb8644ab5257d06c74a45672f14cd73fa5
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:13:20 -0700

    Add lesson 5-60 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// I was previously familiar with the != operator.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
