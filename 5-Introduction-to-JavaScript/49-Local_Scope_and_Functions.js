1b43b5be91e3d254e847a56a438c0f5a2078718f
Carlos Liam <carlos@aarzee.me>
2019-04-05 13:59:26 -0700

    Add lesson 5-49 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/49-Local_Scope_and_Functions.js
@@ -0,0 +1,13 @@
+// I was previously familiar with locally scoped variables.
+
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar = 10;
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+// Now remove the console log line to pass the test
