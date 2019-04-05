538fae82c5aa6cc7e520b55a97c3384368c69315
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:11:51 -0700

    Add lesson 5-58 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,12 @@
+// I was previously familiar with the === operator.
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
