7ca1f6d79180ce68770f99398059b2123922b9d2
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:11:18 -0700

    Add lesson 5-57 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// I was previously familiar with the == operator and type coercion.
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
