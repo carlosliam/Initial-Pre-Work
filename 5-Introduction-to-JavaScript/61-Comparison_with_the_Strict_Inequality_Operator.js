6fc8a9cf76fdda363f5955b33b35a5463322f22c
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:13:59 -0700

    Add lesson 5-61 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+// I was previously familiar with the !== operator.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
