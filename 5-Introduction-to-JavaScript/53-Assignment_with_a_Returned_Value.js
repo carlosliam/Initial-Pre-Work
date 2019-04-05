80fe49789156d31507972eb238fc145b050ad0c9
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:04:40 -0700

    Add lesson 5-53 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/53-Assignment_with_a_Returned_Value.js
@@ -0,0 +1,21 @@
+// I was previously familiar with assigning variables to functions' return values.
+
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7);
