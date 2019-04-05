2968f40d975ae2b99c61c1eb7be4434f1b8b2792
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:12:42 -0700

    Add lesson 5-59 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+// I was previously familiar with using === to compare two values.
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
