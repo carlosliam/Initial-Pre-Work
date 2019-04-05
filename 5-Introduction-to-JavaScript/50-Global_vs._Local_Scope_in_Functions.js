9c096af75b3052f81fd750d131bb7eb9012fa952
Carlos Liam <carlos@aarzee.me>
2019-04-05 14:00:30 -0700

    Add lesson 5-50 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,15 @@
+// I was previously familiar with overriding variables with scopes.
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
