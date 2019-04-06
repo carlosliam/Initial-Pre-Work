d9462f3a9802d524d5016e6832348cc12473c6e4
Carlos Liam <carlos@aarzee.me>
2019-04-06 12:19:40 -0700

    Add lesson 5-101 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// I was previously familiar with Math.random().
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
