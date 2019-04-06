9c24433120e4a13e1876e133408be696e5e4af1b
Carlos Liam <carlos@aarzee.me>
2019-04-06 11:46:38 -0700

    Add lesson 5-80 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// I was previously familiar with JS objects.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  name: "Chompy",
+  legs: 4,
+  tails: 1,
+  friends: ["me!", "johnny"]
+};
