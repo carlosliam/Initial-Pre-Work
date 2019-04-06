63a8dc95bfff9b12ee9d63727bd6f978422da84a
Carlos Liam <carlos@aarzee.me>
2019-04-06 12:00:49 -0700

    Add lesson 5-89 and comment about what I learned

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// I was previously familiar with complex objects using arrays.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Daft Punk",
+    "title": "Random Access Memories",
+    "release_year": 2013,
+    "formats": [
+      "CD",
+      "streaming"
+    ],
+    "gold": true
+  }
+];
