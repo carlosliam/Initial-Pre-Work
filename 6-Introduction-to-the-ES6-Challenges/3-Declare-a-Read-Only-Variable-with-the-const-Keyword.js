6558beadb8248153cc9a735641f1ecf20a9d5ab6
Carlos Liam <carlos@aarzee.me>
2019-04-06 13:03:58 -0700

    Add lesson 6-3 and comment about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,16 @@
+// I was previously familiar with the const keyword and the UPPERCASE convention for constants.
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
