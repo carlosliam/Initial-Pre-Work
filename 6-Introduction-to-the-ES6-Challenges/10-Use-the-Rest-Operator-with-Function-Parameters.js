99aea9e8c3931abc2267a1db33b0cb011f39522d
Carlos Liam <carlos@aarzee.me>
2019-04-06 13:31:54 -0700

    Add lesson 6-10 and comment about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,9 @@
+// I was previously familiar with the ... operator for function arguments.
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
