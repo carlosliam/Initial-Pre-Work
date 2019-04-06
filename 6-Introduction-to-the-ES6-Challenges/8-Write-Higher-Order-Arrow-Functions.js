54cc13e07ecb301523e2a4586c789d48ea943983
Carlos Liam <carlos@aarzee.me>
2019-04-06 13:27:58 -0700

    Add lesson 6-8 and comment about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -0,0 +1,14 @@
+// I was previously familiar with higher order functions such as filter, map, and reduce.
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter((num) => num % 1 === 0 && num > 0).map((num) => num * num);
+
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
