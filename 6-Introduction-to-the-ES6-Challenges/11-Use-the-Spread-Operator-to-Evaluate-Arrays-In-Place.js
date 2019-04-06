fbbd5d909759d75441473aea298302b068633e21
Carlos Liam <carlos@aarzee.me>
2019-04-06 13:33:53 -0700

    Add lesson 6-11 and comment about what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+// I was previously familiar with the ... operator outside of function arguments.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
